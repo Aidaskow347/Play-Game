86d21d95aebfd1985da0020d2d743a6d46b40b27
Aidaskow347 <Aidaskow347@ed.amdsb.ca>
2023-02-09 14:12:43 -0500

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start = new System.Windows.Forms.Button();
+            this.Countinglabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.Start.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(170, 92);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(129, 69);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Countinglabel
+            // 
+            this.Countinglabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Countinglabel.Location = new System.Drawing.Point(132, 115);
+            this.Countinglabel.Name = "Countinglabel";
+            this.Countinglabel.Size = new System.Drawing.Size(221, 23);
+            this.Countinglabel.TabIndex = 1;
+            this.Countinglabel.Text = "Counting Down From: 3";
+            this.Countinglabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Countinglabel.UseMnemonic = false;
+            this.Countinglabel.Visible = false;
+            this.Countinglabel.Click += new System.EventHandler(this.Countinglabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(477, 253);
+            this.Controls.Add(this.Countinglabel);
+            this.Controls.Add(this.Start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Play Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label Countinglabel;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player = new SoundPlayer(Properties.Resources.Beep_Sound);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Countinglabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Start.Visible = false;
+            Countinglabel.Visible = true;
+            Refresh();
+
+            player.Play();
+
+            Thread.Sleep(1250);
+            Countinglabel.Text = "Counting Down From: 2";
+            player.Play();
+            Refresh();
+
+            Thread.Sleep(1250);
+            Countinglabel.Text = "Counting Down From: 1";
+            player.Play();
+            Refresh();
+
+            Thread.Sleep(1250);
+            player = new SoundPlayer(Properties.Resources.Ping_Sound);
+            player.Play();
+            Countinglabel.Text = "Go!!!";
+
+            this.BackColor = Color.LimeGreen;
+        }
+    }
+}
